c260b41d8c8306138e059eb76c03ae5166156811
Pragyaditya Das <pragyaditya.das94@gmail.com>
2014-06-23 14:26:19 +0530

    ruby 1.9.3

---
+++ Variables.rb
@@ -0,0 +1,16 @@
+students = 500 #total people in the class
+space_in_a_class = 20 #space in a class
+teachers = 10 #total number of teachers.
+classes = 20
+teachers_to_student_ratio = 20
+students_not_taken_care_of= students-200
+classes_not_attended=classes-teachers
+
+
+puts "There are #{students} students available."
+puts "There are only #{teachers} teachers available."
+puts "The  teacher to student ratio #{teachers_to_student_ratio}. "
+puts "We cannot take care of #{students_not_taken_care_of} ."
+puts "The classes left unattended are #{classes_not_attended} ."
+
+#use of variables.
